e8617deed9953e13f645611fe52fc1d42b3064b0
Jackson Pires <jackson.pires@gmail.com>
2018-07-27 23:43:32 +0000

    Modulo 03 - Rotas e REST

--- modulo_03/crypo_wallet/config/routes.rb
+++ modulo_03/crypo_wallet/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   resources :coins
+  
+  root to: 'welcome#index'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
