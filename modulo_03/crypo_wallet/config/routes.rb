678eb89f83b5021f039e822d42dedc4d29606d6d
Jackson Pires <jackson.pires@gmail.com>
2018-07-28 00:01:29 +0000

    Modulo 03 - Um pouco mais sobre Rotas

--- modulo_03/crypo_wallet/config/routes.rb
+++ modulo_03/crypo_wallet/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get 'welcome/index'
+  #get '/inicio', to: 'welcome#index'
+
   resources :coins
+  #get '/coins', to: 'coins#index'
   
   root to: 'welcome#index'
   
