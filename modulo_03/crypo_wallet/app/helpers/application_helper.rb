28ac955485661f5a62f3c7d0edf6ee6c17e0f748
Jackson Pires <jackson.pires@gmail.com>
2018-07-30 23:26:38 +0000

    Modulo 03 - Identificando os ambientes no Rails

--- modulo_03/crypo_wallet/app/helpers/application_helper.rb
+++ modulo_03/crypo_wallet/app/helpers/application_helper.rb
@@ -2,4 +2,14 @@ module ApplicationHelper
     def data_br(data_us)
         data_us.strftime("%d/%m/%Y")
     end
+    
+    def ambiente_rails
+        if Rails.env.development?
+            "Desenvolvimento"
+        elsif Rails.env.production?
+            "Produção"
+        else
+            "Teste"
+        end    
+    end
 end
