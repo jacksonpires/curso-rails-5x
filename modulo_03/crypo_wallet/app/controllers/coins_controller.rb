525b8bcf69e9d1a76902d197b8d4e0c6d6c386a6
Jackson Pires <jackson.pires@gmail.com>
2018-09-01 23:20:20 +0000

    Modulo 03 - Conhecendo o helper 'select'

--- modulo_03/crypo_wallet/app/controllers/coins_controller.rb
+++ modulo_03/crypo_wallet/app/controllers/coins_controller.rb
@@ -71,6 +71,6 @@ class CoinsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def coin_params
-      params.require(:coin).permit(:description, :acronym, :url_image)
+      params.require(:coin).permit(:description, :acronym, :url_image, :mining_type_id)
     end
 end
