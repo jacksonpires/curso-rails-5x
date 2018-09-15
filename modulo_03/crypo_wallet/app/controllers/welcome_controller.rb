e9418c6c693151ae0724cafb9a48e476c908f754
Jackson Pires <jackson.pires@gmail.com>
2018-09-15 19:30:04 +0000

    Modulo 03 - Cookies e Sessions

--- modulo_03/crypo_wallet/app/controllers/welcome_controller.rb
+++ modulo_03/crypo_wallet/app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+    cookies[:curso] = "Curso de Ruby on Rails - Jackson Pires [COOKIE]"
+    session[:curso] = "Curso de Ruby on Rails - Jackson Pires [SESSION]"
     @meu_nome = params[:nome]
     @curso = params[:curso]
   end
