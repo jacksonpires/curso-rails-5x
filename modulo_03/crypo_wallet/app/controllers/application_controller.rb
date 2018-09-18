9d876b96ce92113f085857e30b1b71b18c4136bf
Jackson Pires <jackson.pires@gmail.com>
2018-09-18 22:30:10 +0000

    Modulo 03 - Conhecendo Javascript em 1 aula

--- modulo_03/crypo_wallet/app/controllers/application_controller.rb
+++ modulo_03/crypo_wallet/app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+  
+  def set_locale
+    if params[:locale]
+      cookies[:locale] = params[:locale]
+    end
+    
+    if cookies[:locale]
+      if I18n.locale != cookies[:locale]
+        I18n.locale = cookies[:locale]
+      end
+    end
+  end
 end
