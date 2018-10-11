d09d3a955a1d6ff1cac8812ddc14996e0ea2cfc9
Jackson Pires <jackson.pires@gmail.com>
2018-10-11 01:34:13 +0000

    Modulo 03 - Publicando nossa aplicação (Deploy)

--- modulo_03/crypo_wallet/config/environments/production.rb
+++ modulo_03/crypo_wallet/config/environments/production.rb
@@ -1,4 +1,6 @@
 Rails.application.configure do
+  # Verifies that versions and hashed value of the package contents in the project's package.json
+  config.webpacker.check_yarn_integrity = false
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -23,7 +25,7 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = Uglifier.new(harmony: true, compress: { unused: false })
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
