fc7c11301dd59b4471c2ce5b6c4e006a64225f0a
Jackson Pires <jackson.pires@gmail.com>
2018-11-09 22:16:42 -0300

    Modulo 05 - Criando os logins Admin e Profile

--- modulo_05/time_to_answer/app/controllers/welcome_controller.rb
+++ modulo_05/time_to_answer/app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
-  http_basic_authenticate_with name: "jackson", password: "123"
+  # http_basic_authenticate_with name: "jackson", password: "123"
 
   def index
   end
--- modulo_05/time_to_answer/config/routes.rb
+++ modulo_05/time_to_answer/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :profiles
+  devise_for :admins
   get 'welcome/index'
   get 'inicio', to: 'welcome#index'
 
