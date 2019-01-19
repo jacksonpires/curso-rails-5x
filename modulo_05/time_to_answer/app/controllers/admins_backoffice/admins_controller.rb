4366e84843caaa06167acb8edc09597a6ad0a9d6
Jackson Pires <jackson.pires@gmail.com>
2019-01-19 17:57:34 -0300

    Modulo 05 - Finalizando o formulário de edição do Admin

--- modulo_05/time_to_answer/app/controllers/admins_backoffice/admins_controller.rb
+++ modulo_05/time_to_answer/app/controllers/admins_backoffice/admins_controller.rb
@@ -6,4 +6,15 @@ class AdminsBackoffice::AdminsController < AdminsBackofficeController
   def edit
     @admin = Admin.find(params[:id])
   end
+
+  def update
+    admin = Admin.find(params[:id])
+    params_admin = params.require(:admin).permit(:email, :password, :password_confirmation)
+    
+    if admin.update(params_admin)
+      redirect_to admins_backoffice_admins_path, notice: "Administrador atualizado com sucesso!"
+    else
+      render :edit
+    end
+  end
 end
--- modulo_05/time_to_answer/config/routes.rb
+++ modulo_05/time_to_answer/config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   end
   namespace :admins_backoffice do
     get 'welcome/index' # Dashboard
-    resources :admins,  only: [:index, :edit] # Administradores
+    resources :admins,  only: [:index, :edit, :update] # Administradores
   end
   
   devise_for :admins
