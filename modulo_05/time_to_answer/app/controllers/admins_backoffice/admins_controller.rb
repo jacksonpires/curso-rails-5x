881c00d5c976436e4aad3732d30f0ec248db8801
Jackson Pires <jackson.pires@gmail.com>
2019-01-26 15:13:04 -0300

    Modulo 05 - Mostrando os erros do formulário

--- modulo_05/time_to_answer/app/controllers/admins_backoffice/admins_controller.rb
+++ modulo_05/time_to_answer/app/controllers/admins_backoffice/admins_controller.rb
@@ -8,10 +8,10 @@ class AdminsBackoffice::AdminsController < AdminsBackofficeController
   end
 
   def update
-    admin = Admin.find(params[:id])
+    @admin = Admin.find(params[:id])
     params_admin = params.require(:admin).permit(:email, :password, :password_confirmation)
     
-    if admin.update(params_admin)
+    if @admin.update(params_admin)
       redirect_to admins_backoffice_admins_path, notice: "Administrador atualizado com sucesso!"
     else
       render :edit
