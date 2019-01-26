17b2d2b460f4ee5620ddbb1cab03bda7e2a8bdf1
Jackson Pires <jackson.pires@gmail.com>
2019-01-26 16:40:33 -0300

    Modulo 05 - Refatorando o controller

--- modulo_05/time_to_answer/app/controllers/admins_backoffice/admins_controller.rb
+++ modulo_05/time_to_answer/app/controllers/admins_backoffice/admins_controller.rb
@@ -1,24 +1,35 @@
 class AdminsBackoffice::AdminsController < AdminsBackofficeController
+  before_action :verify_password, only: [:update]
+  before_action :set_admin, only: [:edit, :update]
+
   def index
     @admins = Admin.all
   end
 
   def edit
-    @admin = Admin.find(params[:id])
   end
 
-  def update
-    if params[:admin][:password].blank? && params[:admin][:password_confirmation].blank?
-      params[:admin].extract!(:password, :password_confirmation)
-    end
-
-    @admin = Admin.find(params[:id])
-    params_admin = params.require(:admin).permit(:email, :password, :password_confirmation)
-    
+  def update    
     if @admin.update(params_admin)
       redirect_to admins_backoffice_admins_path, notice: "Administrador atualizado com sucesso!"
     else
       render :edit
+    end
+  end
+
+  private
+  
+  def params_admin
+    params.require(:admin).permit(:email, :password, :password_confirmation)
+  end
+
+  def set_admin
+    @admin = Admin.find(params[:id])
+  end
+
+  def verify_password
+    if params[:admin][:password].blank? && params[:admin][:password_confirmation].blank?
+      params[:admin].extract!(:password, :password_confirmation)
     end
   end
 end
