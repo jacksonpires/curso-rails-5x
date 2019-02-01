c38b0b480cf0fe490be37d333bd8b00c0318f6b4
Jackson Pires <jackson.pires@gmail.com>
2019-01-31 22:20:52 -0300

    Modulo 05 - Apagando um Administrador

--- modulo_05/time_to_answer/app/controllers/admins_backoffice/admins_controller.rb
+++ modulo_05/time_to_answer/app/controllers/admins_backoffice/admins_controller.rb
@@ -1,6 +1,6 @@
 class AdminsBackoffice::AdminsController < AdminsBackofficeController
   before_action :verify_password, only: [:update]
-  before_action :set_admin, only: [:edit, :update]
+  before_action :set_admin, only: [:edit, :update, :destroy]
 
   def index
     @admins = Admin.all
@@ -27,6 +27,14 @@ class AdminsBackoffice::AdminsController < AdminsBackofficeController
       redirect_to admins_backoffice_admins_path, notice: "Administrador atualizado com sucesso!"
     else
       render :edit
+    end
+  end
+
+  def destroy
+    if @admin.destroy
+      redirect_to admins_backoffice_admins_path, notice: "Administrador excluído com sucesso!"
+    else
+      render :index
     end
   end
 
--- modulo_05/time_to_answer/config/routes.rb
+++ modulo_05/time_to_answer/config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   end
   namespace :admins_backoffice do
     get 'welcome/index' # Dashboard
-    resources :admins,  except: [:delete] # Administradores
+    resources :admins # Administradores
   end
   
   devise_for :admins
