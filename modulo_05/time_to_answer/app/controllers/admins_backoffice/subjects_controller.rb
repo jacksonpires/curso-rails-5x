c9929ee01ac602eabd347e62d971706a17f5c343
Jackson Pires <jackson.pires@gmail.com>
2019-02-16 16:12:56 -0300

    Modulo 05 -  Criando o controller para Subjects

---
+++ modulo_05/time_to_answer/app/controllers/admins_backoffice/subjects_controller.rb
@@ -0,0 +1,50 @@
+class AdminsBackoffice::SubjectsController < AdminsBackofficeController
+  before_action :set_subject, only: [:edit, :update, :destroy]
+
+  def index
+    @subjects = Subject.all.page(params[:page])
+  end
+
+  def new
+    @subject = Subject.new
+  end
+
+  def create
+    @subject = Subject.new(params_subject)
+    if  @subject.save
+      redirect_to admins_backoffice_subjects_path, notice: "Assunto/Área cadastrado com sucesso!"
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update    
+    if  @subject.update(params_subject)
+      redirect_to admins_backoffice_subjects_path, notice: "Assunto/Área atualizado com sucesso!"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if  @subject.destroy
+      redirect_to admins_backoffice_subjects_path, notice: "Assunto/Área excluído com sucesso!"
+    else
+      render :index
+    end
+  end
+
+  private
+  
+  def params_subject
+    params.require(:subject).permit(:description)
+  end
+
+  def set_subject
+    @subject = Subject.find(params[:id])
+  ends
+end
+
--- modulo_05/time_to_answer/config/routes.rb
+++ modulo_05/time_to_answer/config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
   end
   namespace :admins_backoffice do
     get 'welcome/index' # Dashboard
-    resources :admins # Administradores
+    resources :admins   # Administradores
+    resources :subjects # Assuntos/Áreas
   end
   
   devise_for :admins
