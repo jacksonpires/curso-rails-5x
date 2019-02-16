52e7d6571771b97d69aff5a38feec2d6f4a0a095
Jackson Pires <jackson.pires@gmail.com>
2019-02-16 17:36:30 -0300

    Modulo 05 - Criando as views (new e edit) para os Assuntos_Áreas

--- modulo_05/time_to_answer/app/controllers/admins_backoffice/subjects_controller.rb
+++ modulo_05/time_to_answer/app/controllers/admins_backoffice/subjects_controller.rb
@@ -2,7 +2,7 @@ class AdminsBackoffice::SubjectsController < AdminsBackofficeController
   before_action :set_subject, only: [:edit, :update, :destroy]
 
   def index
-    @subjects = Subject.all.page(params[:page])
+    @subjects = Subject.all.order(:description).page(params[:page])
   end
 
   def new
