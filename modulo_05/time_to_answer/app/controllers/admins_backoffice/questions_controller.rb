72d31430d026d007320f291ac13dac1003fe9fc5
Jackson Pires <jackson.pires@gmail.com>
2019-02-22 20:59:45 -0300

    Modulo 05 - Criando o controller para Questions

---
+++ modulo_05/time_to_answer/app/controllers/admins_backoffice/questions_controller.rb
@@ -0,0 +1,49 @@
+class AdminsBackoffice::QuestionsController < AdminsBackofficeController
+  before_action :set_question, only: [:edit, :update, :destroy]
+
+  def index
+    @questions = Question.all.order(:description).page(params[:page])
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(params_question)
+    if  @question.save
+      redirect_to admins_backoffice_question_path, notice: "Questão cadastrada com sucesso!"
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update    
+    if  @question.update(params_question)
+      redirect_to admins_backoffice_question_path, notice: "Questão atualizado com sucesso!"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if  @question.destroy
+      redirect_to admins_backoffice_questions_path, notice: "Questão excluída com sucesso!"
+    else
+      render :index
+    end
+  end
+
+  private
+  
+  def params_question
+    params.require(:question).permit(:description, :subject_id)
+  end
+
+  def set_question
+    @question = Question.find(params[:id])
+  end
+end
---
+++ modulo_05/time_to_answer/app/helpers/admins_backoffice/questions_helper.rb
@@ -0,0 +1,2 @@
+module AdminsBackoffice::QuestionsHelper
+end
