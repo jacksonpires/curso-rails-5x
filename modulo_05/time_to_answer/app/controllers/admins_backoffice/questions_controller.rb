4d3dd20bd31cbd2dad7771dfe101c3a1e17bba4f
Jackson Pires <jackson.pires@gmail.com>
2019-02-23 14:45:09 -0300

    Modulo 05 - Criando as views (new e edit) para as Questões

--- modulo_05/time_to_answer/app/controllers/admins_backoffice/questions_controller.rb
+++ modulo_05/time_to_answer/app/controllers/admins_backoffice/questions_controller.rb
@@ -12,7 +12,7 @@ class AdminsBackoffice::QuestionsController < AdminsBackofficeController
   def create
     @question = Question.new(params_question)
     if  @question.save
-      redirect_to admins_backoffice_question_path, notice: "Questão cadastrada com sucesso!"
+      redirect_to admins_backoffice_questions_path, notice: "Questão cadastrada com sucesso!"
     else
       render :new
     end
@@ -23,7 +23,7 @@ class AdminsBackoffice::QuestionsController < AdminsBackofficeController
 
   def update    
     if  @question.update(params_question)
-      redirect_to admins_backoffice_question_path, notice: "Questão atualizado com sucesso!"
+      redirect_to admins_backoffice_questions_path, notice: "Questão atualizado com sucesso!"
     else
       render :edit
     end
