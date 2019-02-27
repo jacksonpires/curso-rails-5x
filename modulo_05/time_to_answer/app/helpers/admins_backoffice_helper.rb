7cd8ff92b87d5613bd04de33a495330a250a5b11
Jackson Pires <jackson.pires@gmail.com>
2019-02-27 17:56:18 -0300

    Modulo 05 - Incrementando o i18n para os Models

--- modulo_05/time_to_answer/app/helpers/admins_backoffice_helper.rb
+++ modulo_05/time_to_answer/app/helpers/admins_backoffice_helper.rb
@@ -1,2 +1,5 @@
 module AdminsBackofficeHelper
+  def translate_attribute(object = nil, method = nil)
+    (object && method) ? object.model.human_attribute_name(method) : "Informe os parâmetros corretamente!"
+  end
 end
