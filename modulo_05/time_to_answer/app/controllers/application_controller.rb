64b2547b0cc16aefba0f225b101d36066d2f44e5
Jackson Pires <jackson.pires@gmail.com>
2019-04-04 15:11:26 -0300

    Modulo 05 - Conhecendo o devise_controller e o resource_class

--- modulo_05/time_to_answer/app/controllers/application_controller.rb
+++ modulo_05/time_to_answer/app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  layout :layout_by_resource
+
+  protected
+
+    def layout_by_resource
+      if devise_controller? && resource_class == Admin
+        "admin_devise"
+      else
+        "application"
+      end
+    end
+
 end
