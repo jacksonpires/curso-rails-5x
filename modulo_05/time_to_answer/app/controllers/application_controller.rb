1d2b47cd714697531c8b1bf389b70244519053d9
Jackson Pires <jackson.pires@gmail.com>
2019-04-13 13:33:35 -0300

    Modulo 05 - Uma pitada de metaprogramação no login Devise User

--- modulo_05/time_to_answer/app/controllers/application_controller.rb
+++ modulo_05/time_to_answer/app/controllers/application_controller.rb
@@ -4,11 +4,6 @@ class ApplicationController < ActionController::Base
   protected
 
     def layout_by_resource
-      if devise_controller? && resource_class == Admin
-        "admin_devise"
-      else
-        "application"
-      end
+      devise_controller? ? "#{resource_class.to_s.downcase}_devise" : "application"
     end
-
 end
