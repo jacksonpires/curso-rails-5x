c8613173db9fec59bc1bbdf8377920f7a109ad69
Jackson Pires <jackson.pires@gmail.com>
2019-04-26 10:30:14 -0300

    Modulo 05 - Listando as perguntas e respostas na index

--- modulo_05/time_to_answer/app/controllers/site/welcome_controller.rb
+++ modulo_05/time_to_answer/app/controllers/site/welcome_controller.rb
@@ -1,4 +1,5 @@
 class Site::WelcomeController < SiteController
   def index
+    @questions = Question.includes(:answers).page(params[:page])
   end
 end
--- modulo_05/time_to_answer/app/models/question.rb
+++ modulo_05/time_to_answer/app/models/question.rb
@@ -2,4 +2,7 @@ class Question < ApplicationRecord
   belongs_to :subject, inverse_of: :questions
   has_many :answers
   accepts_nested_attributes_for :answers, reject_if: :all_blank, allow_destroy: true
+
+   # Kaminari
+   paginates_per 5
 end
