c735a38ddd06d2a50bbd3d40c19bd88909260437
Jackson Pires <jackson.pires@gmail.com>
2019-04-29 21:24:34 -0300

    Modulo 05 - Fazendo a pesquisa funcionar

--- modulo_05/time_to_answer/app/controllers/site/search_controller.rb
+++ modulo_05/time_to_answer/app/controllers/site/search_controller.rb
@@ -1,5 +1,7 @@
 class Site::SearchController < SiteController
   def questions
-    @questions = Question.includes(:answers).page(params[:page])
+    @questions = Question.includes(:answers)
+                         .where("lower(description) LIKE ?", "%#{params[:term].downcase}%")
+                         .page(params[:page])
   end
 end
