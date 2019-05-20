63cb2d447d05f9bae6499a87455d591d7848bc8e
Jackson Pires <jackson.pires@gmail.com>
2019-05-20 20:59:39 -0300

    Modulo 05 - Adicionando o nome do usuário no Devise

--- modulo_05/time_to_answer/app/models/user.rb
+++ modulo_05/time_to_answer/app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  
+  def full_name
+    [self.first_name, self.last_name].join(' ')
+  end
 end
--- modulo_05/time_to_answer/db/schema.rb
+++ modulo_05/time_to_answer/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_14_183034) do
+ActiveRecord::Schema.define(version: 2019_05_20_223938) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -56,6 +56,8 @@ ActiveRecord::Schema.define(version: 2019_05_14_183034) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
