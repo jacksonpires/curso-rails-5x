338415bafbe2d5d66a5bdfe487afcf7181735488
Jackson Pires <jackson.pires@gmail.com>
2019-05-21 22:16:17 -0300

    Modulo 05 - Conhecendo e usando validações no Rails

--- modulo_05/time_to_answer/app/models/user.rb
+++ modulo_05/time_to_answer/app/models/user.rb
@@ -5,6 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   
+  # Validations
+  validates :first_name, presence: true, length: { minimum: 3 }, on: :update
+  
+  # Virtual Attributes
   def full_name
     [self.first_name, self.last_name].join(' ')
   end
