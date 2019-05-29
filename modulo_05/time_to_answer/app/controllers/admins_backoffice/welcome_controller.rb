8457a805f8eef7aa4add1f1fe4f33e672623d74f
Jackson Pires <jackson.pires@gmail.com>
2019-05-29 19:29:58 -0300

    Modulo 05 - Refatorando o AdminStatistic

--- modulo_05/time_to_answer/app/controllers/admins_backoffice/welcome_controller.rb
+++ modulo_05/time_to_answer/app/controllers/admins_backoffice/welcome_controller.rb
@@ -1,6 +1,6 @@
 class AdminsBackoffice::WelcomeController < AdminsBackofficeController
   def index
-    @total_users = AdminStatistic.find_by_event(AdminStatistic::EVENTS[:total_users]).value
-    @total_questions = AdminStatistic.find_by_event(AdminStatistic::EVENTS[:total_questions]).value
+    @total_users = AdminStatistic.total_users
+    @total_questions = AdminStatistic.total_questions
   end
 end
--- modulo_05/time_to_answer/app/models/admin_statistic.rb
+++ modulo_05/time_to_answer/app/models/admin_statistic.rb
@@ -4,6 +4,16 @@ class AdminStatistic < ApplicationRecord
     total_questions: "TOTAL_QUESTIONS"
   }
 
+  # Scopes
+  scope :total_users, -> {
+    find_by_event(EVENTS[:total_users]).value
+  }
+
+  scope :total_questions, -> {
+    find_by_event(EVENTS[:total_questions]).value
+  }
+
+  # Class Methods
   def self.set_event(event)
     admin_statistic = AdminStatistic.find_or_create_by(event: event)
     admin_statistic.value += 1
