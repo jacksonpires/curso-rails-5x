fe7fa925f327920324e21e1cced54520510ec307
Jackson Pires <jackson.pires@gmail.com>
2019-05-29 12:50:48 -0300

    Modulo 05 - Forçando o usuário a efetuar login

--- modulo_05/time_to_answer/app/controllers/application_controller.rb
+++ modulo_05/time_to_answer/app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationController < ActionController::Base
   layout :layout_by_resource
+  before_action :check_pagination
 
   protected
 
     def layout_by_resource
       devise_controller? ? "#{resource_class.to_s.downcase}_devise" : "application"
+    end
+
+    def check_pagination
+      unless user_signed_in?
+        params.extract!(:page)
+      end
     end
 end
