72ce4280fad3d4b62d7af88b9a31bcd1f348f557
Jackson Pires <jackson.pires@gmail.com>
2019-05-29 17:16:17 -0300

    Modulo 05 - Refatorando o UserStatistic

--- modulo_05/time_to_answer/app/controllers/site/answer_controller.rb
+++ modulo_05/time_to_answer/app/controllers/site/answer_controller.rb
@@ -1,16 +1,6 @@
 class Site::AnswerController < SiteController
   def question
     @answer = Answer.find(params[:answer_id])
-    set_user_statistic(@answer)
-  end
-
-  private
-
-  def set_user_statistic(answer)
-    if user_signed_in?
-      user_statistic = UserStatistic.find_or_create_by(user: current_user)
-      answer.correct? ? user_statistic.right_questions += 1 : user_statistic.wrong_questions += 1
-      user_statistic.save
-    end
+    UserStatistic.set_statistic(@answer, current_user)
   end
 end
--- modulo_05/time_to_answer/app/models/user_statistic.rb
+++ modulo_05/time_to_answer/app/models/user_statistic.rb
@@ -5,4 +5,13 @@ class UserStatistic < ApplicationRecord
   def total_questions
     self.right_questions + self.wrong_questions
   end
+
+  # Class Methods
+  def self.set_statistic(answer, current_user)
+    if !!current_user
+      user_statistic = UserStatistic.find_or_create_by(user: current_user)
+      answer.correct? ? user_statistic.right_questions += 1 : user_statistic.wrong_questions += 1
+      user_statistic.save
+    end
+  end
 end
