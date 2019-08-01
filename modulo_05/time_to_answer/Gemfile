b6c3d96264f014dfe64e8d1e32b7e62da9139486
Jackson Pires <jackson.pires@gmail.com>
2019-08-01 14:48:16 -0300

    Modulo 07 - Configurando o Redis no Rails

--- modulo_05/time_to_answer/Gemfile
+++ modulo_05/time_to_answer/Gemfile
@@ -44,6 +44,8 @@ gem 'kaminari'
 gem 'kaminari-i18n'
 gem "cocoon"
 gem 'prawn-rails'
+gem 'redis'
+gem 'hiredis'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- modulo_05/time_to_answer/config/environments/development.rb
+++ modulo_05/time_to_answer/config/environments/development.rb
@@ -28,18 +28,8 @@ Rails.application.configure do
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
-  if Rails.root.join('tmp', 'caching-dev.txt').exist?
-    config.action_controller.perform_caching = true
-
-    config.cache_store = :memory_store
-    config.public_file_server.headers = {
-      'Cache-Control' => "public, max-age=#{2.days.to_i}"
-    }
-  else
-    config.action_controller.perform_caching = false
-
-    config.cache_store = :null_store
-  end
+  
+  config.cache_store = :redis_cache_store
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
